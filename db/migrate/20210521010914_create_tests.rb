2cc49d709f2c97a7a08b664135fe8835af90d22b
maxim <maximsandulsky@gmail.com>
2021-05-22 06:10:53 +0300

    Initializing tables and adding default values by migration

--- db/migrate/20210521010914_create_tests.rb
+++ db/migrate/20210521010914_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.references :category, null: false, foreign_key: true
-      t.integer :author, null: false
+      t.references :user, null: false, foreign_key: true
       t.integer :level, null: false, default: 0
       t.string :title, null: false
 
