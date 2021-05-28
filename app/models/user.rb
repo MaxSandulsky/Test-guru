1e8927919e8a86b2d017b71e342b0e87c9576a42
maxim <maximsandulsky@gmail.com>
2021-05-28 20:32:34 +0300

    Intermediate table PassedTest added, active_tests method reforged

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :tests, inverse_of: 'author', foreign_key: 'author_id'
-
-  #  def active_tests(level)
-  #    Test.where(level: level, id: tests)
-  #  end
+  has_many :passed_tests
+  has_many :test_passed, through: :passed_tests, source: :tests
+  
+  def active_tests(level)
+    test_passed.order(:id).where(level: level)
+  end
 end
