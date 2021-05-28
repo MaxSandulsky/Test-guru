041310382ad8a21912a98df63a28ac8ca8efc934
maxim <maximsandulsky@gmail.com>
2021-05-28 20:33:32 +0300

    rubocop apply

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :tests, inverse_of: 'author', foreign_key: 'author_id'
   has_many :passed_tests
   has_many :test_passed, through: :passed_tests, source: :tests
-  
+
   def active_tests(level)
     test_passed.order(:id).where(level: level)
   end
