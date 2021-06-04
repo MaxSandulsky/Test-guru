7e18327fa194f040c655a258fcf8359e944122ac
maxim <maximsandulsky@gmail.com>
2021-06-04 16:58:50 +0300

    Associations added

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,35 +4,35 @@ categories = Category.create!([{ title: 'Backend' }, { title: 'Frontend' }, { ti
 users = User.create!([{ login: 'OpenUser', password: 'doAos11-', e_mail: 'sample@mailer.com' },
                       { login: 'Max', password: 'sample', e_mail: 'max@mailer.com' }])
 
-tests = Test.create!([{ category_id: categories[0].id, author_id: users[0].id, title: 'Ruby for beginners' },
-                      { category_id: categories[1].id, author_id: users[0].id, title: 'HTML for the little ones' },
-                      { category_id: categories[0].id, author_id: users[1].id, title: 'Advanced Rails', level: 1 }])
+tests = Test.create!([{ category: categories[0], author: users[0], title: 'Ruby for beginners' },
+                      { category: categories[1], author: users[0], title: 'HTML for the little ones' },
+                      { category: categories[0], author: users[1], title: 'Advanced Rails', level: 1 }])
 
-questions = Question.create!([{ test_id: tests[0].id, body: 'In Ruby, everything is ...' },
-                              { test_id: tests[0].id,
+questions = Question.create!([{ test: tests[0], body: 'In Ruby, everything is ...' },
+                              { test: tests[0],
                                 body: 'What function do you need to print output to the screen?' },
-                              { test_id: tests[0].id, body: 'What symbol do you need to commentate the line?' },
-                              { test_id: tests[0].id, body: 'How to define variable type in Ruby?' },
-                              { test_id: tests[0].id, body: 'How to define constant in Ruby?' }])
+                              { test: tests[0], body: 'What symbol do you need to commentate the line?' },
+                              { test: tests[0], body: 'How to define variable type in Ruby?' },
+                              { test: tests[0], body: 'How to define constant in Ruby?' }])
 
-answers = Answer.create!([{ question_id: questions[0].id, body: 'function' },
-                          { question_id: questions[0].id, body: 'number' },
-                          { question_id: questions[0].id, body: 'object', correct: true },
-                          { question_id: questions[1].id, body: 'window' },
-                          { question_id: questions[1].id, body: 'outputs' },
-                          { question_id: questions[1].id, body: 'puts', correct: true },
-                          { question_id: questions[2].id, body: '@' },
-                          { question_id: questions[2].id, body: '//' },
-                          { question_id: questions[2].id, body: '#', correct: true },
-                          { question_id: questions[3].id, body: 'Define type before variable' },
-                          { question_id: questions[3].id, body: 'Simply write "var" before variable' },
-                          { question_id: questions[3].id, body: 'Variable type will be determine automatically',
+answers = Answer.create!([{ question: questions[0], body: 'function' },
+                          { question: questions[0], body: 'number' },
+                          { question: questions[0], body: 'object', correct: true },
+                          { question: questions[1], body: 'window' },
+                          { question: questions[1], body: 'outputs' },
+                          { question: questions[1], body: 'puts', correct: true },
+                          { question: questions[2], body: '@' },
+                          { question: questions[2], body: '//' },
+                          { question: questions[2], body: '#', correct: true },
+                          { question: questions[3], body: 'Define type before variable' },
+                          { question: questions[3], body: 'Simply write "var" before variable' },
+                          { question: questions[3], body: 'Variable type will be determine automatically',
                             correct: true },
-                          { question_id: questions[4].id, body: 'Write "const" before constant' },
-                          { question_id: questions[4].id, body: 'Write "static" before constant' },
-                          { question_id: questions[4].id, body: 'Capitalize constants', correct: true }])
+                          { question: questions[4], body: 'Write "const" before constant' },
+                          { question: questions[4], body: 'Write "static" before constant' },
+                          { question: questions[4], body: 'Capitalize constants', correct: true }])
 
-ptests = PassedTest.create!([{ user_id: users[0].id, test_id: tests[0].id },
-                             { user_id: users[0].id, test_id: tests[1].id },
-                             { user_id: users[0].id, test_id: tests[2].id },
-                             { user_id: users[1].id, test_id: tests[0].id }])
+ptests = PassedTest.create!([{ user: users[0], test: tests[0] },
+                             { user: users[0], test: tests[1] },
+                             { user: users[0], test: tests[2] },
+                             { user: users[1], test: tests[0] }])
