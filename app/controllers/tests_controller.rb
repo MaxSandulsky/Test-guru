0851ecedf77e1227adb9dd626c78ad932036b7ee
maxim <maximsandulsky@gmail.com>
2021-06-12 20:58:18 +0300

    TestController fix

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -19,7 +19,7 @@ class TestsController < ApplicationController
   def new; end
 
   def create
-    @test = Test.build(test_params)
+    @test = Test.new(test_params)
     if @test.save
       redirect_to @test
     else
