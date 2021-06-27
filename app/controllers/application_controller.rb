557e5f9994deb3b83ed602d853de242e8fdadbf5
maxim <maximsandulsky@gmail.com>
2021-06-27 14:45:27 +0300

    byebug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def authenticate_user!
     unless current_user
       cookies[:target_path] = request.path_info
-byebug
+
       redirect_to login_path, alert: 'Verify Email or Password'
     end
   end
