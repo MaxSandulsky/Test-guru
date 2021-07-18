f9855635bb15cbab621119546941ca0ec403e989
maxim <maximsandulsky@gmail.com>
2021-07-18 05:23:06 +0300

    tests by first attempt

--- app/specifications/badges/badges_rules.rb
+++ app/specifications/badges/badges_rules.rb
@@ -3,7 +3,11 @@ module BadgesRules
     (user.tests_passed & Test.by_category(value)).size == Test.by_category(value).size
   end
 
-  def self.test_by_first_attempt(value, user)
+  def self.tests_by_first_attempt(user)
+    Test.all.select { |test| test.test_passages.where(user: user).count == 1 }.empty?
+  end
+
+  def self.specific_test_first_attempt(value, user)
     user.tests_passed.where(id: value).count == 1
   end
 
